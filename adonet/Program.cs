061f84a8842739ac1980ccffd6f78f89e88d8242
endok <keisuke.endo9@gmail.com>
2017-12-30 16:54:05 +0900

    DataReaderの例を追加。

--- adonet/Program.cs
+++ adonet/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,34 @@ namespace adonet
     {
         static void Main(string[] args)
         {
+            string connectionString = "Data Source=(local);Initial Catalog=testDB;User ID=sa;Password=P@ssw0rd";
+
+            using (SqlConnection connection = new SqlConnection())
+            {
+                connection.ConnectionString = connectionString;
+                connection.Open();
+
+                WriteRows(connection);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void WriteRows(SqlConnection connection)
+        {
+            string query = "SELECT id, name, age FROM member WHERE age >= @age ";
+            int paramAge = 25;
+
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@age", paramAge);
+
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine("id={0},name={1},age={2}", reader[0], reader[1], reader[2]);
+            }
+            reader.Close();
+
         }
     }
 }
