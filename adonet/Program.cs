627c2492d711e7b3c26e421157ae223aa5e73ccd
endok <keisuke.endo9@gmail.com>
2018-01-01 01:49:55 +0900

    DataSetの例を追加。

--- adonet/Program.cs
+++ adonet/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -18,19 +19,29 @@ namespace adonet
                 connection.ConnectionString = connectionString;
                 connection.Open();
 
-                WriteRows(connection);
+                Console.WriteLine("----- SqlCommand -----");
+
+                // SqlCommandによる参照
+                SelectRowsWithSqlCommand(connection);
+                // SqlCommandによる更新
+                UpdateWithSqlCommand(connection);
+
+                Console.WriteLine("----- DataSet -----");
+
+                // DataSetによる参照、更新
+                SelectAndUpdateRowsWithDataSet(connection);
+
             }
 
             Console.ReadLine();
         }
 
-        private static void WriteRows(SqlConnection connection)
+        private static void SelectRowsWithSqlCommand(SqlConnection connection)
         {
             string query = "SELECT id, name, age FROM member WHERE age >= @age ";
-            int paramAge = 25;
 
             SqlCommand command = new SqlCommand(query, connection);
-            command.Parameters.AddWithValue("@age", paramAge);
+            command.Parameters.AddWithValue("@age", 25);
 
             SqlDataReader reader = command.ExecuteReader();
             while (reader.Read())
@@ -39,6 +50,57 @@ namespace adonet
             }
             reader.Close();
 
+        }
+
+        private static void UpdateWithSqlCommand(SqlConnection connection)
+        {
+            string query = "UPDATE member SET age = 40 WHERE id = @id";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@id", 3);
+
+            command.ExecuteNonQuery();
+        }
+
+        private static void SelectAndUpdateRowsWithDataSet(SqlConnection connection)
+        {
+            string selectQuery = "SELECT id, name, age FROM member";
+            string updateQuery = "UPDATE member SET age = @age WHERE id = @id";
+
+            SqlDataAdapter adapter = new SqlDataAdapter();
+
+            // 主キー情報をDBから取得
+            adapter.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+
+            // クエリの設定
+            adapter.SelectCommand = new SqlCommand(selectQuery, connection);
+            adapter.UpdateCommand = new SqlCommand(updateQuery, connection);
+
+            // Updateパラメータの設定
+            SqlParameter ageParam = new SqlParameter();
+            ageParam.ParameterName = "@age";
+            ageParam.SourceColumn = "age";
+            adapter.UpdateCommand.Parameters.Add(ageParam);
+            SqlParameter idParam = new SqlParameter();
+            idParam.ParameterName = "@id";
+            idParam.SourceColumn = "id";
+            adapter.UpdateCommand.Parameters.Add(idParam);
+
+            DataSet dataset = new DataSet();
+            adapter.Fill(dataset, "memberTable");
+            DataTable datatable = dataset.Tables["memberTable"];
+
+            // データの参照
+            foreach (DataRow row in datatable.Rows)
+            {
+                Console.WriteLine("id={0},name={1},age={2}", row["id"], row["name"], row["age"]);
+            }
+
+            // データの更新
+            DataRow carolRow = datatable.Rows.Find(3);
+            carolRow["age"] = 30;
+
+            adapter.Update(dataset, "memberTable");
         }
     }
 }
---
+++ adonetdataset/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adonetdataset
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Data Source=(local);Initial Catalog=testDB;User ID=sa;Password=P@ssw0rd";
+
+            using (SqlConnection connection = new SqlConnection())
+            {
+                connection.ConnectionString = connectionString;
+                connection.Open();
+
+                WriteRows(connection);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void WriteRows(SqlConnection connection)
+        {
+            string query = "SELECT id, name, age FROM member WHERE age >= @age ";
+            int paramAge = 25;
+
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@age", paramAge);
+
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine("id={0},name={1},age={2}", reader[0], reader[1], reader[2]);
+            }
+            reader.Close();
+
+        }
+    }
+}
