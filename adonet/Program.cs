fc83151dd62d5826af0525e67d31f0c083d1403a
endok <keisuke.endo9@gmail.com>
2018-01-02 13:31:34 +0900

    EntityFrameworkを追加して全体構成を調整。

--- adonet/Program.cs
+++ adonet/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.Common;
+using System.Data.Entity.Core.EntityClient;
+using System.Data.Entity.Core.Objects;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -10,97 +13,154 @@ namespace adonet
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            string connectionString = "Data Source=(local);Initial Catalog=testDB;User ID=sa;Password=P@ssw0rd";
+            const string connectionString = "Data Source=(local);Initial Catalog=testDB;User ID=sa;Password=P@ssw0rd";
 
+            Console.WriteLine("----- SqlCommand -----");
+            SqlCommandSample(connectionString);
+
+            Console.WriteLine("----- DataSet -----");
+            DataSetSample(connectionString);
+
+            Console.WriteLine("----- EntityFramework -----");
+            EntityFrameworkSample();
+            
+            Console.ReadLine();
+        }
+
+        private static void SqlCommandSample(string connectionString)
+        {
             using (SqlConnection connection = new SqlConnection())
             {
                 connection.ConnectionString = connectionString;
                 connection.Open();
 
-                Console.WriteLine("----- SqlCommand -----");
-
                 // SqlCommandによる参照
-                SelectRowsWithSqlCommand(connection);
-                // SqlCommandによる更新
-                UpdateWithSqlCommand(connection);
+                string selectquery = "SELECT id, name, age FROM member WHERE age >= @age ";
 
-                Console.WriteLine("----- DataSet -----");
+                SqlCommand selectcommand = new SqlCommand(selectquery, connection);
+                selectcommand.Parameters.AddWithValue("@age", 25);
+
+                SqlDataReader reader = selectcommand.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine("id={0},name={1},age={2}", reader[0], reader[1], reader[2]);
+                }
+                reader.Close();
+
+                // SqlCommandによる更新
+                string updatequery = "UPDATE member SET age = 40 WHERE id = @id";
+
+                SqlCommand updatecommand = new SqlCommand(updatequery, connection);
+                updatecommand.Parameters.AddWithValue("@id", 3);
+
+                updatecommand.ExecuteNonQuery();
+            }
+        }
+
+        private static void DataSetSample(string connectionString)
+        {
+            using (SqlConnection connection = new SqlConnection())
+            {
+                connection.ConnectionString = connectionString;
+                connection.Open();
 
                 // DataSetによる参照、更新
-                SelectAndUpdateRowsWithDataSet(connection);
+                string selectQuery = "SELECT id, name, age FROM member";
+                string updateQuery = "UPDATE member SET age = @age WHERE id = @id";
 
+                SqlDataAdapter adapter = new SqlDataAdapter();
+
+                // 主キー情報をDBから取得
+                adapter.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+
+                // クエリの設定
+                adapter.SelectCommand = new SqlCommand(selectQuery, connection);
+                adapter.UpdateCommand = new SqlCommand(updateQuery, connection);
+
+                // Updateパラメータの設定
+                SqlParameter ageParam = new SqlParameter();
+                ageParam.ParameterName = "@age";
+                ageParam.SourceColumn = "age";
+                adapter.UpdateCommand.Parameters.Add(ageParam);
+                SqlParameter idParam = new SqlParameter();
+                idParam.ParameterName = "@id";
+                idParam.SourceColumn = "id";
+                adapter.UpdateCommand.Parameters.Add(idParam);
+
+                DataSet dataset = new DataSet();
+                adapter.Fill(dataset, "memberTable");
+                DataTable datatable = dataset.Tables["memberTable"];
+
+                // データの参照
+                foreach (DataRow row in datatable.Rows)
+                {
+                    Console.WriteLine("id={0},name={1},age={2}", row["id"], row["name"], row["age"]);
+                }
+
+                // データの更新
+                DataRow carolRow = datatable.Rows.Find(3);
+                carolRow["age"] = 50;
+
+                adapter.Update(dataset, "memberTable");
             }
 
-            Console.ReadLine();
         }
 
-        private static void SelectRowsWithSqlCommand(SqlConnection connection)
+        private static void EntityFrameworkSample()
         {
-            string query = "SELECT id, name, age FROM member WHERE age >= @age ";
-
-            SqlCommand command = new SqlCommand(query, connection);
-            command.Parameters.AddWithValue("@age", 25);
-
-            SqlDataReader reader = command.ExecuteReader();
-            while (reader.Read())
+            using (var db = new SampleContext())
             {
-                Console.WriteLine("id={0},name={1},age={2}", reader[0], reader[1], reader[2]);
+                // LINQ to Entitiesでの参照
+                Console.WriteLine("-- LINQ to Entities");
+                var query1 = from m in db.member
+                             where m.age >= 25
+                             select m;
+
+                foreach (var member in query1)
+                {
+                    Console.WriteLine("id={0},name={1},age={2}", member.id, member.name, member.age);
+                }
+
+                // 別の書き方での参照
+                Console.WriteLine("-- LINQ to Entities 2");
+                var query2 = db.member.Where(m => m.age >= 25);
+
+                foreach (var member in query2)
+                {
+                    Console.WriteLine("id={0},name={1},age={2}", member.id, member.name, member.age);
+                }
+
+                // Entity Framewokでの更新
+                var carol = db.member.Find(3);
+                carol.age = 30;
+                db.SaveChanges();
             }
-            reader.Close();
 
-        }
-
-        private static void UpdateWithSqlCommand(SqlConnection connection)
-        {
-            string query = "UPDATE member SET age = 40 WHERE id = @id";
-
-            SqlCommand command = new SqlCommand(query, connection);
-            command.Parameters.AddWithValue("@id", 3);
-
-            command.ExecuteNonQuery();
-        }
-
-        private static void SelectAndUpdateRowsWithDataSet(SqlConnection connection)
-        {
-            string selectQuery = "SELECT id, name, age FROM member";
-            string updateQuery = "UPDATE member SET age = @age WHERE id = @id";
-
-            SqlDataAdapter adapter = new SqlDataAdapter();
-
-            // 主キー情報をDBから取得
-            adapter.MissingSchemaAction = MissingSchemaAction.AddWithKey;
-
-            // クエリの設定
-            adapter.SelectCommand = new SqlCommand(selectQuery, connection);
-            adapter.UpdateCommand = new SqlCommand(updateQuery, connection);
-
-            // Updateパラメータの設定
-            SqlParameter ageParam = new SqlParameter();
-            ageParam.ParameterName = "@age";
-            ageParam.SourceColumn = "age";
-            adapter.UpdateCommand.Parameters.Add(ageParam);
-            SqlParameter idParam = new SqlParameter();
-            idParam.ParameterName = "@id";
-            idParam.SourceColumn = "id";
-            adapter.UpdateCommand.Parameters.Add(idParam);
-
-            DataSet dataset = new DataSet();
-            adapter.Fill(dataset, "memberTable");
-            DataTable datatable = dataset.Tables["memberTable"];
-
-            // データの参照
-            foreach (DataRow row in datatable.Rows)
+            // Entity SQLでの参照
+            Console.WriteLine("-- Entity SQL");
+            using (EntityConnection connection = new EntityConnection("name=SampleContext"))
             {
-                Console.WriteLine("id={0},name={1},age={2}", row["id"], row["name"], row["age"]);
+                connection.Open();
+
+                string query1 = "SELECT m.id, m.name, m.age FROM SampleContext.member AS m WHERE m.age >= @age";
+
+                using (EntityCommand cmd = new EntityCommand(query1, connection))
+                {
+                    cmd.Parameters.AddWithValue("age", 25);
+
+                    using (DbDataReader rdr = cmd.ExecuteReader(CommandBehavior.SequentialAccess))
+                    {
+                        while (rdr.Read())
+                        {
+                            Console.WriteLine("id={0},name={1},age={2}", rdr["id"], rdr["name"], rdr["age"]);
+                        }
+                    }
+                }
+
             }
-
-            // データの更新
-            DataRow carolRow = datatable.Rows.Find(3);
-            carolRow["age"] = 30;
-
-            adapter.Update(dataset, "memberTable");
         }
     }
 }
